a57333b9ab6dbd8c8095259a5d33b098c3dd58c4
loleksatyr <czarnotabartek@gmail.com>
2021-06-10 19:12:08 +0200

    Creating_project

---
+++ Agent_demo/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] int _agentNumber;
+ 
+    [SerializeField] 
+    [Range(2,10)]
+    float _agenttime;
+
+    [SerializeField] 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

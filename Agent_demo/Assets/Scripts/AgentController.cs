0a59d51d033d12ca451c9b0432ed70954d3a86f1
loleksatyr <czarnotabartek@gmail.com>
2021-06-10 19:30:30 +0200

    Creating Agent Movement

--- Agent_demo/Assets/Scripts/AgentController.cs
+++ Agent_demo/Assets/Scripts/AgentController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class AgentController : MonoBehaviour
 {
+    [SerializeField] int MoveSpeed;
+    [SerializeField] int Health;
+    [SerializeField] float number;
     void Start()
     {
-        
+        MoveSpeed = 4;
+        Health = 3;
+        number = Random.Range(-15f, 15f);
+        transform.Rotate(0, number, 0, Space.Self);
     }
 
 
     void Update()
     {
-        
+        checkedges();
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+    }
+    private void checkedges()
+    {
+        bool edge = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 2);
+        if (edge == false)
+        {
+            transform.Rotate(0, number, 0, Space.Self);
+        }
     }
 }
